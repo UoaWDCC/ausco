3c56ea801e44b97f856b64ea0e03ebe541674524
HarryShengCS <harryshengcoding@gmail.com>
2025-05-12 20:30:07 +1200

    chore: added hero section

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,23 +1,15 @@
-import { getLandingPage } from "@/actions/getLandingPage";
-import { getAllTest } from "@/actions/getTest";
-import config from "@/payload.config";
-
 import Header from "@components/home/Header";
 import UpcomingConcert from "@components/home/UpcomingConcert";
 import InfoCards from "@components/home/InfoCards";
 import PastConcert from "@components/home/PastConcert";
 import Footer from "@components/home/Footer";
+import HeroSection from "@components/home/HeroSection";
 
 export default async function HomePage() {
-  const [payloadConfig, content, testItems] = await Promise.all([
-    config,
-    getLandingPage(),
-    getAllTest(),
-  ]);
-
   return (
     <>
       <Header />
+      <HeroSection />
       <UpcomingConcert />
       <InfoCards />
       <PastConcert />
