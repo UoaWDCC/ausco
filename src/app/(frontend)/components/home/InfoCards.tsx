722908e0ad49119ac2511a7ea342865db3f91ddf
del-ereno <delereno2@gmail.com>
2025-05-14 01:12:48 +1200

    feat:flex and links

--- src/app/(frontend)/components/home/InfoCards.tsx
+++ src/app/(frontend)/components/home/InfoCards.tsx
@@ -3,25 +3,37 @@ const cardData = [
     title: "About Us",
     description:
       "This is a description about the page, in a few sentences. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+    linkText:"Read more↗",
+    linkHref:""
   },
   {
     title: "Our People",
     description:
       "This is a description about the page, in a few sentences. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+    linkText:"Read more↗",
+    linkHref:""
   },
   {
     title: "Reach Out",
     description: "Here are a few ways you can reach out to us:",
+    linkText:"Feedback Form↗",
+    linkHref:"",
+    contacts:[
+      {text:"chamberorchestra.ausa@gmail.com",href:""},
+      {text:"@ausco.uoa",href:""},
+      {text:"@ausco.ausa",href:""}
+    ]
   },
 ];
 const InfoCards = () => {
   return (
     <section>
-      <div className="gap-6">
+      <div className="flex gap-6">
         {cardData.map((card, i) => (
-          <div key={i}>
-            <h2>{card.title}</h2>
-            <p>{card.description}</p>
+          <div key={i} className="p-6">
+            <h2 className="">{card.title}</h2>
+            <p className="">{card.description}</p>
+            <a href={card.linkHref}>{card.linkText}</a>
           </div>
         ))}
       </div>
