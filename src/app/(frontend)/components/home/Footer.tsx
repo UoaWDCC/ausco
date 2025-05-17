e88879fec5e07440f4105251d3ccf14ec9225d18
h4yleysh4rpe <sharpeh42@gmail.com>
2025-05-17 15:38:09 +1200

    feat: add basic footer structure

--- src/app/(frontend)/components/home/Footer.tsx
+++ src/app/(frontend)/components/home/Footer.tsx
@@ -1,9 +1,29 @@
 const Footer = () => {
-    return (
-      <footer>
-        <p>Footer content</p>
-      </footer>
-    );
-  };
-  
+  return (
+    <footer>
+      <div className="footer-box">
+        <div>
+          <div className="logo-box"></div>
+          <div>
+            <div className="footer-title">Auckland University Chamber Orchestra Inc.</div>
+            <div className="socials"></div>
+          </div>
+        </div>
+        <div className="documents-box">
+          Documents
+          <p>Footer content</p>
+        </div>
+        <div className="joinus-box">
+          Join us
+          <p>Sign up form</p>
+        </div>
+        <div className="reachout-box">
+          Reach out to us
+          <p>Feedback form</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
 export default Footer;
