af540e80291fe3d6b34c96295a0fccbac22aaa3d
Patricia <pg.reyes@outlook.com>
2025-05-19 00:00:19 +1200

    feat: added placeholder logo and centred text

--- src/app/(frontend)/components/home/HeroSection.tsx
+++ src/app/(frontend)/components/home/HeroSection.tsx
@@ -3,8 +3,7 @@ import { getAllTest } from "@/actions/getTest";
 import config from "@/payload.config";
 
 import {Button} from "../ui/button";
-/*import logo from "../../assets/ausco-logo-1.png";*/
-import heroImage from "../../assets/hero.jpg";
+import logo from "../../assets/ausco-logo-1.png";
 
 const HeroSection = async () => {
     const [payloadConfig, content, testItems] = await Promise.all([
@@ -18,11 +17,12 @@ const HeroSection = async () => {
       <div className="absolute inset-0 bg-[url('./assets/hero.jpg')] bg-cover bg-no-repeat bg-[center_bottom_0%] z-0"></div>
       <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.6)] to-[rgba(0,0,0,0.6)] z-10"></div>
       <div className="relative z-20 home">
+        <img src={logo.src} alt="AUSCO logo" className="w-1/3 h=auto"/>
       <div className="content gap-8">
         {content && (
           <>
             <h1>{content.header.title}</h1>
-            <h3>{content.header.content}</h3>
+            <h3 className="text-center">{content.header.content}</h3>
           </>
         )}
         <div className="links">
@@ -45,7 +45,7 @@ const HeroSection = async () => {
           ))}
         </div>
         <Button>Click me</Button>
-      </div>
+    </div>
     </div>
     </div>
     );
