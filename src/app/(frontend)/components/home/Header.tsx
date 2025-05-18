608df1734671290984ef46c1772a12e3707806c1
jinlong <jshe426@aucklanduni.ac.nz>
2025-05-18 20:55:30 +1200

    feat: logo + header buttons

--- src/app/(frontend)/components/home/Header.tsx
+++ src/app/(frontend)/components/home/Header.tsx
@@ -1,9 +1,25 @@
+"use client";
 const Header = () => {
-    return (
-      <header>
-        <h1>Header section</h1>
-      </header>
-    );
-  };
-  
+  return (
+    <header>
+      <nav className="flex items-center justify-between pt-6 pr-12 pl-12">
+        <div className="flex items-center space-x-4">
+          <img src="/app/(frontend)/assets/hero.jpg" />
+          <span className="text-xl font-medium">
+            Auckland University <br />
+            Student Chamber Orchestra Inc.
+          </span>
+        </div>
+        <div className="flex items-center gap-12">
+          <button>About Us</button>
+          <button>Our People</button>
+          <button>Concerts</button>
+          <button>Gallery</button>
+          <button>Contact Us</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
 export default Header;
