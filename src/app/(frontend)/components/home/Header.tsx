124235358d60dba41973052ae5a1d726a678e3fa
jinlong <jshe426@aucklanduni.ac.nz>
2025-05-19 21:30:59 +1200

    fix: fixed image path

--- src/app/(frontend)/components/home/Header.tsx
+++ src/app/(frontend)/components/home/Header.tsx
@@ -1,21 +1,23 @@
 "use client";
+import Image from "next/image";
+import auscologo from "../../assets/ausco-logo-1.png";
 const Header = () => {
   return (
     <header>
       <nav className="flex items-center justify-between pt-6 pr-12 pl-12">
         <div className="flex items-center space-x-4">
-          <img src="/app/(frontend)/assets/hero.jpg" />
-          <span className="text-xl font-medium">
+          <Image src={auscologo} alt="AUSCO logo" width={60} height={60} />
+          <span className="text-lg font-medium">
             Auckland University <br />
             Student Chamber Orchestra Inc.
           </span>
         </div>
         <div className="flex items-center gap-12">
-          <button>About Us</button>
-          <button>Our People</button>
-          <button>Concerts</button>
-          <button>Gallery</button>
-          <button>Contact Us</button>
+          <button className="text-sm">About Us</button>
+          <button className="text-sm">Our People</button>
+          <button className="text-sm">Concerts</button>
+          <button className="text-sm">Gallery</button>
+          <button className="text-sm">Contact Us</button>
         </div>
       </nav>
     </header>
