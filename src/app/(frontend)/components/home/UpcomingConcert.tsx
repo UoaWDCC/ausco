3f51da4b667d9091667ea0d03fb1c8ca6ad1c9ce
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-05-22 00:21:07 +1200

    heading with styles

--- src/app/(frontend)/components/home/UpcomingConcert.tsx
+++ src/app/(frontend)/components/home/UpcomingConcert.tsx
@@ -3,9 +3,9 @@ import UpcomingConcertPoster from "../../assets/upcoming-concert.jpg";
 const UpcomingConcert = () => {
     return (
       <section>
-        <div className="UpcomingTitle">
-          <h1>Our Upcoming Concert,</h1>
-          <h1> Till Death Do Us Apart</h1>
+        <div className="items-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+          <h1 className="inline-block mr-4">Our Upcoming Concert,</h1> 
+          <h1 className="inline-block italic"> Till Death Do Us Apart</h1>
         </div>
         <img src="{ UpcomingConcertPoster }" alt="Till Death Do Us Apart Concert Poster" />
       </section>
