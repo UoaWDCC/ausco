8a608934960f2e4d0637970ddb850f8798eb3529
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-05-22 10:20:41 +1200

    fixed text width

--- src/app/(frontend)/components/home/UpcomingConcert.tsx
+++ src/app/(frontend)/components/home/UpcomingConcert.tsx
@@ -2,18 +2,20 @@ import UpcomingConcertPoster from "../../assets/upcoming-concert.jpg";
 
 const UpcomingConcert = () => {
     return (
-      <section className="bg-[#eee5d8] text-[#602c0f] text-center mx-auto">
-        <div className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
+      <section className="bg-[#eee5d8] text-[#602c0f] mx-auto">
+
+        <div className="text-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
           <p className="inline-block mr-4">Our Upcoming Concert,</p> 
           <p className="inline-block italic"> Till Death Do Us 'Part</p>
         </div>
-        <div className="flex flex-row gap-17 justify-center items-center mt-4">
+
+        <div className="flex flex-row gap-16 justify-center mt-4">
           <img 
           src={ UpcomingConcertPoster.src } 
           alt="Till Death Do Us Apart Concert Poster" 
-          className="w-80 h-auto mr-4 border-2 border-[#602c0f] rounded-md" />
-          <div className="w-120 text-sm">
-            <p >Love, Passion, and Loss. Our Semester 1 concert delves into the trials and the tribulations of romance and its all-encompassing nature - as well as the grief we feel in its absence</p>
+          className="w-80 h-auto mr-4 border-2 border-[#602c0f] rounded-md mt-12" />
+          <div className="w-96 text-sm text-left mt-0">
+            <p className="mt-0">Love, Passion, and Loss. Our Semester 1 concert delves into the trials and the tribulations of romance and its all-encompassing nature - as well as the grief we feel in its absence</p>
             <p>That's why we partnered with Tōtara Hospice, in support of providing accessible, quality, and compassionate palliative care in their last moments. All concert proceeds will go towards this cause. Join us at one of our two concerts!</p>
             <hr className="border-t-[1.5px] border-[#602c0f] my-4" />
           </div>
