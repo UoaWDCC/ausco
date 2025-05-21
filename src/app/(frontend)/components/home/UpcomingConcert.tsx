e454d66e39c0b31154b43224879b6bbe720548be
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-05-22 01:37:19 +1200

    fixed gap between image and text

--- src/app/(frontend)/components/home/UpcomingConcert.tsx
+++ src/app/(frontend)/components/home/UpcomingConcert.tsx
@@ -7,12 +7,15 @@ const UpcomingConcert = () => {
           <p className="inline-block mr-4">Our Upcoming Concert,</p> 
           <p className="inline-block italic"> Till Death Do Us Apart</p>
         </div>
-        <div className="flex flex-row justify-center items-center mt-4">
+        <div className="flex flex-row gap-17 justify-center items-center mt-4">
           <img 
           src={ UpcomingConcertPoster.src } 
           alt="Till Death Do Us Apart Concert Poster" 
           className="w-80 h-auto mr-4 border-2 border-[#602c0f] rounded-md" />
-          <p className="w-120 text-sm">Love, Passion, and Loss. Our Semester 1 concert delves into the trials </p>
+          <div>
+            <p className="w-120 text-sm">Love, Passion, and Loss. Our Semester 1 concert delves into the trials and the tribulations of romance and its all-encompassing nature - as well as the grief we feel in its absence</p>
+
+          </div>
         </div>
         
       </section>
