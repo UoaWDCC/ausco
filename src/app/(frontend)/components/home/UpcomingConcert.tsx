af1268b42cca57e12dd79abcad4badae3f9da847
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-05-22 00:31:53 +1200

    text color

--- src/app/(frontend)/components/home/UpcomingConcert.tsx
+++ src/app/(frontend)/components/home/UpcomingConcert.tsx
@@ -2,7 +2,7 @@ import UpcomingConcertPoster from "../../assets/upcoming-concert.jpg";
 
 const UpcomingConcert = () => {
     return (
-      <section className="bg-[#eee5d8]">
+      <section className="bg-[#eee5d8] text-[#602c0f]">
         <div className="items-center text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">
           <h1 className="inline-block mr-4">Our Upcoming Concert,</h1> 
           <h1 className="inline-block italic"> Till Death Do Us Apart</h1>
