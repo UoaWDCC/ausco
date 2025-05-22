36dc5b441b6170700ccc9b0e4bca38d835ae87d1
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-05-22 12:34:43 +1200

    decrease font size

--- src/app/(frontend)/components/home/UpcomingConcert.tsx
+++ src/app/(frontend)/components/home/UpcomingConcert.tsx
@@ -14,7 +14,7 @@ const UpcomingConcert = () => {
           src={ UpcomingConcertPoster.src } 
           alt="Till Death Do Us Apart Concert Poster" 
           className="w-80 h-auto mr-4 border-2 border-[#602c0f] rounded-md mt-6" />
-          <div className="w-96 text-sm text-left mt-0 pt-0">
+          <div className="w-96 text-xs text-left mt-0 pt-0">
             <p className="mt-0 pt-0">Love, Passion, and Loss. Our Semester 1 concert delves into the trials and the tribulations of romance and its all-encompassing nature - as well as the grief we feel in its absence</p>
             <p className="mb-4 ">That's why we partnered with Tōtara Hospice, in support of providing accessible, quality, and compassionate palliative care in their last moments. All concert proceeds will go towards this cause. Join us at one of our two concerts!</p>
             <hr className="border-t-[1.5px] border-[#602c0f] mt-12" />
