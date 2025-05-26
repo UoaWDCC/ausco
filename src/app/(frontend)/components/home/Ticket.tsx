baa1d76869cf949400abb6d94d5f33c37b18eb40
santi145 <hannahvillanuevasantiago@gmail.com>
2025-05-26 22:04:05 +1200

    Feat: AUSCO-9: Upcoming Concerts section

--- src/app/(frontend)/components/home/Ticket.tsx
+++ src/app/(frontend)/components/home/Ticket.tsx
@@ -1,18 +1,35 @@
+import { getLandingPage } from "@/actions/getLandingPage";
+import config from "@/payload.config";
 import { Button } from "../ui/button";
 import { Calendar, MapPin, ArrowUpRight } from "lucide-react";
 
-const Ticket = () => {
+const getTickets = async () => {
+  return {
+    matinee: {
+      title:  "Matinee",
+      date: "Date TBC",
+      location: "Location TBC",
+      
+
+    }
+  }
+}
+
+const Ticket = async () => {
+  const [payloadConfig, content] = await Promise.all([config, getLandingPage()]);
+
   return (
+
     <section className="flex justify-around p-6">
       {/* Matinee Section */}
       <div className="ticket-section bg-card p-4 rounded w-1/3 text-center">
         <h2 className="text-lg font-bold mb-2 text-[#602C0F]">Matinee</h2>
         <div className="details text-[#602C0F]">
-          <div className="flex items-center justify-center gap-2">
+          <div className="flex items-center gap-2">
             <Calendar size={20} className="stroke-[#602C0F]" />
             <span>Date TBC</span>
           </div>
-          <div className="flex items-center justify-center gap-2 mt-2">
+          <div className="flex items-center gap-2 mt-2">
             <MapPin size={20} className="stroke-[#602C0F]" />
             <span>Location TBC</span>
           </div>
@@ -26,11 +43,11 @@ const Ticket = () => {
       <div className="ticket-section bg-card p-4 rounded w-1/3 text-center">
         <h2 className="text-lg font-bold mb-2 text-[#602C0F]">Concert</h2>
         <div className="details text-[#602C0F]">
-          <div className="flex items-center justify-center gap-2">
+          <div className="flex items-center gap-2">
             <Calendar size={20} className="stroke-[#602C0F]" />
             <span>Date TBC</span>
           </div>
-          <div className="flex items-center justify-center gap-2 mt-2">
+          <div className="flex items-center gap-2 mt-2">
             <MapPin size={20} className="stroke-[#602C0F]" />
             <span>Location TBC</span>
           </div>
