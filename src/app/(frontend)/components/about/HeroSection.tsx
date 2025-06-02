393842d84764433e4c23fefbffaba7a6afe8936b
Jacob-LDY802 <littlejacob2999@gmail.com>
2025-06-02 22:58:20 +1200

    fix: used next/image

--- src/app/(frontend)/components/about/HeroSection.tsx
+++ src/app/(frontend)/components/about/HeroSection.tsx
@@ -1,40 +1,49 @@
+import Image from "next/image";
 
 const circleImages = [
   // 1 o'clock
-  { src: "/assets/about-us-01.jpg", style: "top-1/8 right-1/4 w-40 h-40" },
+  { src: "/assets/about-us-01.jpg", style: "top-1/8 right-1/4 w-40 h-40", width: 160, height: 160 },
   // 2 o'clock
-  { src: "/assets/about-us-01.jpg", style: "top-1/4 right-1/5 w-25 h-16" },
+  { src: "/assets/about-us-01.jpg", style: "top-1/4 right-1/5 w-25 h-16", width: 100, height: 64 },
   // 4 o'clock
-  { src: "/assets/about-us-01.jpg", style: "bottom-1/4 right-1/6 w-25 h-20" },
+  { src: "/assets/about-us-01.jpg", style: "bottom-1/4 right-1/6 w-25 h-20", width: 100, height: 80 },
   // 5 o'clock
-  { src: "/assets/about-us-01.jpg", style: "bottom-1/8 right-1/4 w-40 h-16" },
+  { src: "/assets/about-us-01.jpg", style: "bottom-1/8 right-1/4 w-40 h-16", width: 160, height: 64 },
   // 7 o'clock (normal)
-  { src: "/assets/about-us-01.jpg", style: "bottom-1/6 left-1/4 w-40 h-25" },
+  { src: "/assets/about-us-01.jpg", style: "bottom-1/6 left-1/4 w-40 h-25", width: 160, height: 100 },
   // 7 o'clock (corner)
-  { src: "/assets/about-us-01.jpg", style: "bottom-1/9 left-1/6 w-20 h-16" },
+  { src: "/assets/about-us-01.jpg", style: "bottom-1/9 left-1/6 w-20 h-16", width: 80, height: 64 },
   // 8 o'clock
-  { src: "/assets/about-us-01.jpg", style: "bottom-1/4 left-1/8 w-25 h-20" },
+  { src: "/assets/about-us-01.jpg", style: "bottom-1/4 left-1/8 w-25 h-20", width: 100, height: 80 },
   // 9.5 o'clock
-  { src: "/assets/about-us-01.jpg", style: "top-1/3 left-1/8 w-30 h-20" },
+  { src: "/assets/about-us-01.jpg", style: "top-1/3 left-1/8 w-30 h-20", width: 120, height: 80 },
   // 10 o'clock
-  { src: "/assets/about-us-01.jpg", style: "top-1/4 left-1/5 w-40 h-60" },
+  { src: "/assets/about-us-01.jpg", style: "top-1/4 left-1/5 w-40 h-60", width: 160, height: 240 },
   // 11 o'clock
-  { src: "/assets/about-us-01.jpg", style: "top-1/5 left-1/3 w-25 h-16" },
+  { src: "/assets/about-us-01.jpg", style: "top-1/5 left-1/3 w-25 h-16", width: 100, height: 64 },
 ];
 
 const HeroSection = () => {
   return (
-    <section className="relative flex flex-col items-center justify-center w-full bg-[#F6F4EC]" style={{ height: "100vh", minHeight: "400px" }}
-    >
+    <section className="relative flex flex-col items-center justify-center w-full bg-[#F6F4EC]" style={{ height: "100vh", minHeight: "400px" }}>
       {/* Circle Images */}
       <div className="absolute inset-0 pointer-events-none">
         {circleImages.map((img, i) => (
-          <img
+          <div
             key={i}
-            src={img.src}
-            alt=""
             className={`rounded-md absolute ${img.style}`}
-          />
+            style={{ position: "absolute" }}
+          >
+            <Image
+              src={img.src}
+              alt=""
+              width={img.width}
+              height={img.height}
+              className="rounded-md"
+              priority={i === 0}
+              style={{ objectFit: "cover" }}
+            />
+          </div>
         ))}
       </div>
       <div className="relative z-10 flex flex-col items-center space-y-20">
