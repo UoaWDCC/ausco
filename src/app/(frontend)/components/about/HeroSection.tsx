c10f18f3756c6963f36bc10c292e472bb5151384
Jacob-LDY802 <littlejacob2999@gmail.com>
2025-06-03 20:47:42 +1200

    fix: replaced empty space

--- src/app/(frontend)/components/about/HeroSection.tsx
+++ src/app/(frontend)/components/about/HeroSection.tsx
@@ -50,7 +50,7 @@ const HeroSection = () => {
         <h1 className="text-[#264C84] font-extrabold text-center text-2xl md:text-4xl" style={{ margin: 0 }}>
           About Us
         </h1>
-        <p>" "</p>
+        <p>&quot; &quot;</p>
         <p className="text-[#264C84] text-center leading-relaxed max-w-2xl text-sm md:text-lg" style={{ fontSize: "clamp(0.8rem,1.2vw,1.2rem)" }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
