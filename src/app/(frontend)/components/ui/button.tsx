63d06eabcbfe6abe8577ff0c0e88e1ed5ac2b4ac
vincenttao04 <vincent.jh.tao@gmail.com>
2025-06-04 16:35:00 +1200

    chore: clean line variant on button.tsx

--- src/app/(frontend)/components/ui/button.tsx
+++ src/app/(frontend)/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary: "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-sm mb-1 w-fit relative inline-block after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[1px] after:w-0 after:bg-current after:transition-all after:duration-300 hover:after:w-full",
+        link: "relative inline-block after:absolute after:left-0 after:-bottom-0 after:h-[1px] after:w-0 after:bg-current after:transition-[width] after:duration-300 hover:after:w-full",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
