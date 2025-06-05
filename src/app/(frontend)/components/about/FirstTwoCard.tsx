2f0345e7f9d87ea650d859c8c5d6c1a2595f1ca2
Patricia <pg.reyes@outlook.com>
2025-06-05 23:32:53 +1200

    feat: made responsive

--- src/app/(frontend)/components/about/FirstTwoCard.tsx
+++ src/app/(frontend)/components/about/FirstTwoCard.tsx
@@ -20,7 +20,7 @@ const FirstTwoCard = async () => {
     <div className="w-full bg-[#F6F4EC]">
     <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
     <div className="flex flex-col md:flex-row justify-center items-stretch gap-8">
-      <div className="relative w-full md:w-2/3 lg:w-2/3 aspect-video rounded-lg overflow-hidden shadow-sm">
+      <div className="relative w-full md:w-3/5 lg:w-3/5 aspect-video rounded-lg overflow-hidden shadow-sm">
           {content.visionCard && content.visionCard['background-image'] && (
             <img
             src={getImageUrl(content.visionCard['background-image'])}
@@ -29,13 +29,13 @@ const FirstTwoCard = async () => {
             />
         )}
 
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-[#602C0F] p-6">
-          <Eye size={40} color="#602c0f"/> <h3 className="text-5xl font-serif mb-4">{content.visionCard.title}</h3>
-          <p className="text-base">{content.visionCard["short-desc"]}</p>
+        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-[#602C0F] px-2 py-4 lg:p-16">
+          <Eye color="#602c0f" className="size-[40px] sm:size-[35px]"/> <h3 className="text-3xl lg:text-5xl font-bold my-4">{content.visionCard.title}</h3>
+          <p className="text-sm md:text-base">{content.visionCard["short-desc"]}</p>
         </div>
       </div>
 
-    <div className="relative w-full md:w-1/3 lg:w-1/3 aspect-video rounded-lg overflow-hidden shadow-sm">
+    <div className="relative w-full md:w-2/5 lg:w-2/5 aspect-video rounded-lg overflow-hidden shadow-sm">
           {content.historyCard && content.historyCard['background-image'] && (
             <img
             src={getImageUrl(content.historyCard['background-image'])}
@@ -43,9 +43,9 @@ const FirstTwoCard = async () => {
             className="absolute inset-0 w-full h-full object-cover opacity-15"
             />
         )}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-[#264C84] p-6">
-        <History size={35} color="#264c84"/> <h3 className="text-5xl font-serif mb-4">{content.historyCard.title}</h3>
-        <p className="text-base">{content.historyCard["short-desc"]}</p>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-[#264C84] px-2 py-4 lg:p-16">
+        <History color="#264c84" className="size-[35px] sm:size-[35px]"/> <h3 className="text-3xl lg:text-5xl font-bold my-4">{content.historyCard.title}</h3>
+        <p className="text-sm md:text-base">{content.historyCard["short-desc"]}</p>
       </div>
     </div>
   </div>
