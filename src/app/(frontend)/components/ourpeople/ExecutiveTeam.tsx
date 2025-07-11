03ff7c0371abd9c57708eb5af811ec7016fc942e
del-ereno <delereno2@gmail.com>
2025-07-12 05:36:32 +1200

    feat:added general description + flex w header

--- src/app/(frontend)/components/ourpeople/ExecutiveTeam.tsx
+++ src/app/(frontend)/components/ourpeople/ExecutiveTeam.tsx
@@ -1,8 +1,17 @@
 const ExecutiveTeam = async () => {
   return (
     <div className="w-full bg-[#F6F4EC]">
-      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <h2 className="text-[var(--navy)] text-4xl font-medium">Executive Team</h2>
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 sm:py-8">
+        <div className="flex items-end justify-between gap-4 md:gap-0 text-[var(--navy)]">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium">
+            Executive Team
+          </h2>
+          {/*note for del: add desc text to payload*/}
+          <p className="text-xs  max-w-full md:max-w-lg font-light">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          </p>
+        </div>
       </div>
     </div>
   );
