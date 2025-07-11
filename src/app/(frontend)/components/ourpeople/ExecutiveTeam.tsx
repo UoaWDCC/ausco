34a55a42f18e871693a25a4cb917f64ecc2036c3
del-ereno <delereno2@gmail.com>
2025-07-12 05:19:09 +1200

    feat:setup component w margins

---
+++ src/app/(frontend)/components/ourpeople/ExecutiveTeam.tsx
@@ -0,0 +1,11 @@
+const ExecutiveTeam = async () => {
+  return (
+    <div className="w-full bg-[#F6F4EC]">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h2 className="text-[var(--navy)] text-4xl font-medium">Executive Team</h2>
+      </div>
+    </div>
+  );
+};
+
+export default ExecutiveTeam;
