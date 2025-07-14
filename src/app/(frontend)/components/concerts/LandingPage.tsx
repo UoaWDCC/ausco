204f199f8522643534ea75e9672f2c3b423df0d4
Patricia <pg.reyes@outlook.com>
2025-07-14 19:38:54 +1200

    feat: added basic page layout

---
+++ src/app/(frontend)/components/concerts/LandingPage.tsx
@@ -0,0 +1,27 @@
+import { Media } from "@/payload-types";
+
+const LandingPage = async () => {
+  const getImageUrl = (imageField: string | Media | null | undefined): string | undefined => {
+    if (typeof imageField === "object" && imageField !== null && "url" in imageField) {
+      return imageField.url ?? undefined;
+    }
+    if (typeof imageField === "string") {
+      return imageField;
+    }
+    return undefined; // Return undefined if no image is found
+  };
+  
+    return (
+        <div className="min-h-screen bg-[#F6F4EC] flex flex-col items-center pt-20 md:h-[max(880px,100dvh)]">
+            <h1 className="text-[#602C0F]">Concerts</h1>
+            <div className="flex flex-col md:flex-row justify-center items-stretch gap-8">
+                <div className="relative w-full md:w-3/5 lg:w-3/5 aspect-video rounded-lg overflow-hidden shadow-sm">
+                </div>
+                <div className="relative w-full md:w-2/5 lg:w-2/5 aspect-video rounded-lg overflow-hidden shadow-sm">
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LandingPage;
