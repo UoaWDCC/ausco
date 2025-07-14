b0ebb10658859a275db87f51c3923ec634b32d92
Patricia <pg.reyes@outlook.com>
2025-07-15 00:17:06 +1200

    feat: created cards w/ bg images + overlay

--- src/app/(frontend)/components/concerts/LandingPage.tsx
+++ src/app/(frontend)/components/concerts/LandingPage.tsx
@@ -1,5 +1,9 @@
 import { getConcertsLanding } from "@/actions/getConcertsLanding";
 import { Media } from "@/payload-types";
+import { ArrowUpRight } from "lucide-react";
+import { Archive } from 'lucide-react';
+import { CalendarClock } from 'lucide-react';
+import { CalendarCheck } from 'lucide-react';
 
 const LandingPage = async () => {
   const [content] = await Promise.all([getConcertsLanding()]);
@@ -15,12 +19,38 @@ const LandingPage = async () => {
     };
 
   return (
-    <div className="min-h-screen bg-[#F6F4EC] flex flex-col items-center pt-20 md:h-[max(880px,100dvh)]">
-      <h1 className="text-[#602C0F]">Concerts</h1>
-      <div className="flex flex-col md:flex-row justify-center items-stretch gap-8">
-        <div className="relative w-full md:w-3/5 lg:w-3/5 aspect-video rounded-lg overflow-hidden shadow-sm">
-        </div>
-        <div className="relative w-full md:w-2/5 lg:w-2/5 aspect-video rounded-lg overflow-hidden shadow-sm">
+    <div className="w-full bg-[#F6F4EC] pt-20 md:h-[max(880px,100dvh)]">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <h1 className="text-[#602C0F] text-center">Concerts</h1>
+        <div className="flex flex-col md:flex-row justify-center items-stretch gap-11 pt-10">
+          <div className="relative md:w-1/2 lg:w-1/2 aspect-4/3 rounded-lg overflow-hidden shadow-sm">
+            {content.upcomingCard && content.upcomingCard["background-image"] && (
+              <img
+                src={getImageUrl(content.upcomingCard["background-image"])}
+                alt={
+                  typeof content.upcomingCard["background-image"] === "object"
+                    ? content.upcomingCard["background-image"].alt
+                    : "Upcoming Concerts Background"
+                }
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            )}
+            <div className="absolute inset-0 bg-black opacity-[70%]"></div>
+          </div>
+          <div className="relative md:w-1/2 lg:w-1/2 aspect-4/3 rounded-lg overflow-hidden shadow-sm">
+            {content.pastCard && content.pastCard["background-image"] && (
+              <img
+                src={getImageUrl(content.pastCard["background-image"])}
+                alt={
+                  typeof content.pastCard["background-image"] === "object"
+                    ? content.pastCard["background-image"].alt
+                    : "Past Concerts Background"
+                }
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            )}
+            <div className="absolute inset-0 bg-black opacity-[70%]"></div>
+          </div>
         </div>
       </div>
     </div>
