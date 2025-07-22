ea744e1afcf57e7262886840f4c06f86f604dbe6
del-ereno <delereno2@gmail.com>
2025-07-23 02:02:10 +1200

    feat:basic layout setup for upcoming concert page

---
+++ src/app/(frontend)/components/concerts/upcoming/HeroSection.tsx
@@ -0,0 +1,11 @@
+const HeroSection = async () => {
+  return (
+    <div className="w-full bg-[var(--cream)]  pt-40 pb-20">
+      <div className="max-w-screen-xl mx-auto">
+        <h1 className="text-[var(--brown)] text-center">Upcoming Concerts</h1>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/app/(frontend)/components/concerts/upcoming/SemesterOneConcert.tsx
@@ -0,0 +1,14 @@
+const SemesterOneConcert = async () => {
+  return (
+    <div className="w-full bg-[var(--cream)] pt-20 ">
+      <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <div className="bg-[var(--beige)] text-[var(--brown)] text-center p-4 rounded-lg">
+          <h1>Semester One Concert</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SemesterOneConcert;
