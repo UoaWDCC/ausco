01f7586291b5b8a03597386d1fbb3a6a0402b013
h4yleysh4rpe <sharpeh42@gmail.com>
2025-07-29 21:21:40 +1200

    feat: add headers to other pages

--- src/app/(frontend)/ourpeople/page.tsx
+++ src/app/(frontend)/ourpeople/page.tsx
@@ -1,8 +1,14 @@
+import Header from "@components/home/Header";
 import OurPeople from "@components/ourpeople/OurPeople";
 import ExecutiveTeam from "@components/ourpeople/ExecutiveTeam";
+import { getHeader } from "@/actions/getHeader";
+
+const headerContent = await getHeader();
+
 export default async function OurPeoplePage() {
   return (
     <>
+      <Header content={headerContent} />
       <OurPeople />
       <ExecutiveTeam />
     </>
