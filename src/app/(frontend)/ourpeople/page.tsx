a9e0f93633bde495342d128f849ecabb91aa1585
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-07-31 14:18:25 +1200

    feat: adding section to ourpeople page

--- src/app/(frontend)/ourpeople/page.tsx
+++ src/app/(frontend)/ourpeople/page.tsx
@@ -1,9 +1,11 @@
 import OurPeople from "@components/ourpeople/OurPeople";
+import HallOfFame from "@components/ourpeople/HallOfFame";
 
 export default async function OurPeoplePage() {
   return (
     <>
       <OurPeople />
+      <HallOfFame />
     </>
   );
 }
