3234df31c1a24481359393a6febb684fd15c51ae
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-07-31 14:17:56 +1200

    feat: title of section

---
+++ src/app/(frontend)/components/ourpeople/HallOfFame.tsx
@@ -0,0 +1,12 @@
+import { getOurPeople } from "@/actions/getOurPeoplePage";
+import { Media } from "@/payload-types";
+
+const HallOfFame = async () => {
+    return(
+        <div>
+            <h1> Hall of Fame </h1>
+        </div>
+    );
+}
+
+export default HallOfFame;
