f3cada9236e3e1a9942861b7dd2dd6df62ea5b08
HarryShengCS <harryshengcoding@gmail.com>
2025-08-13 22:13:55 +1200

    feat: added header to past and upcoming concert

--- src/app/(frontend)/concerts/upcoming/page.tsx
+++ src/app/(frontend)/concerts/upcoming/page.tsx
@@ -1,10 +1,15 @@
 import HeroSection from "@components/concerts/upcoming/HeroSection";
 import SemesterOneConcert from "@components/concerts/upcoming/SemesterOneConcert";
 import Calendar from "@components/concerts/upcoming/Calendar";
+import Header from "@components/home/Header";
+import { getHeader } from "@/actions/getHeader";
 
-export default function Upcoming() {
+export default async function Upcoming() {
+  const headerContent = await getHeader();
+
   return (
     <>
+      <Header content={headerContent} />
       <HeroSection />
       <SemesterOneConcert />
       <Calendar />
