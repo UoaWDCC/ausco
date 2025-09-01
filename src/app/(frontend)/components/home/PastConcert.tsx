e5dc7bfe0895303e5a0b8a384ef2b9f47dbc949a
del-ereno <delereno2@gmail.com>
2025-09-01 15:33:02 +1200

    feat: "concertblue" style var added, implemented

--- src/app/(frontend)/components/home/PastConcert.tsx
+++ src/app/(frontend)/components/home/PastConcert.tsx
@@ -81,7 +81,7 @@ const PastConcert = () => {
     >
       <h2
         className="w-full max-w-[90vw] text-[2rem] sm:text-[2.7rem] font-bold text-center tracking-tight mb-2 leading-tight whitespace-nowrap overflow-hidden text-ellipsis"
-        style={{ color: "var(--navy)" }}
+        style={{ color: "var(--concertblue)" }}
       >
         From our last concert
       </h2>
