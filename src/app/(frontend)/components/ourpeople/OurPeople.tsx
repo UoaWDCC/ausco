5c899c90785d5d77933dc8db410eac42ef27b64a
Aname Gloria <102767502+Aname326@users.noreply.github.com>
2025-09-29 23:41:46 +1300

    feat: our people page uploading images

--- src/app/(frontend)/components/ourpeople/OurPeople.tsx
+++ src/app/(frontend)/components/ourpeople/OurPeople.tsx
@@ -6,12 +6,14 @@ const OurPeople = async () => {
   const [content] = await Promise.all([getOurPeople()]);
   return (
     <div className="bg-[var(--cream)] text-[var(--navy)] flex flex-col gap-16 justify-center items-center mx-auto py-9 px-4 lg:flex-row">
-      <Image 
-        src={content.image.url || ""}
-        alt={content.image.alt || "Concert poster"}
-        width={400}
-        height={300}
-      />
+      {content.image && typeof content.image !== "string" ? (
+        <Image 
+          src={content.image.url ?? ""}
+          alt={content.image.alt ?? "Concert poster"}
+          width={400}
+          height={300}
+        />
+      ) : null}
 
       <div className="text-center mt-0 pt-0">
         <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl">Our People</h1>
