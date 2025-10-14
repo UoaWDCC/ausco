2c38c830b9338e46541c8930d55215de7f50b0d0
vincenttao04 <vincent.jh.tao@gmail.com>
2025-10-14 13:48:42 +1300

    chore: clean

--- src/app/(frontend)/components/home/Ticket.tsx
+++ src/app/(frontend)/components/home/Ticket.tsx
@@ -18,7 +18,7 @@ const Ticket = ({ matineeData, concertData }: TicketProps) => {
   return (
     <section className="flex justify-between bg-[#EEE5D8]">
       {/* Matinee Section */}
-      <div className="ticket-section bg-[#EEE5D8] py-4 rounded w-1/3">
+      <div className="ticket-section bg-[#EEE5D8] rounded w-1/3">
         <h2 className="text-lg font-bold mb-2 text-[#602C0F]">{matineeData?.title}</h2>
         <div className="details text-[#602C0F]">
           <div className="flex items-center gap-2">
@@ -36,7 +36,7 @@ const Ticket = ({ matineeData, concertData }: TicketProps) => {
       </div>
 
       {/* Concert Section */}
-      <div className="ticket-section bg-[#EEE5D8] py-4 rounded w-1/3">
+      <div className="ticket-section bg-[#EEE5D8] rounded w-1/3">
         <h2 className="text-lg font-bold mb-2 text-[#602C0F]">{concertData?.title}</h2>
         <div className="details text-[#602C0F]">
           <div className="flex items-center gap-2">
