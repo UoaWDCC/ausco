78dec0974158bf642da37e34c513584f950dc9f1
HarryShengCS <harryshengcoding@gmail.com>
2025-10-20 15:54:52 +1300

    feat(frontend): add Fraunces font

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import Header from "@components/home/Header";
 import { getHeader } from "@/actions/getHeader";
+import { Fraunces } from "next/font/google";
 import "./styles.css";
+
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-fraunces",
+});
 
 export const metadata = {
   description: "A blank template using Payload in a Next.js app.",
@@ -13,8 +20,8 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const headerContent = await getHeader();
 
   return (
-    <html lang="en">
-      <body>
+    <html lang="en" className={fraunces.variable}>
+      <body className={fraunces.className}>
         <Header content={headerContent} />
         <main>{children}</main>
       </body>
