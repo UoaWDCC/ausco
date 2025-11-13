99410d9a65fe6191260914f4f08a9297384a543d
vincenttao04 <vincent.jh.tao@gmail.com>
2025-11-13 15:58:41 +1300

    refactor: next.js image on stickers array

--- src/app/(frontend)/components/about/Hero.tsx
+++ src/app/(frontend)/components/about/Hero.tsx
@@ -8,14 +8,7 @@ type HeroProps = {
   };
 };
 
-const Hero = async ({ content }: HeroProps) => {
-  const getImageUrl = (imageField: string | Media | null | undefined) => {
-    if (typeof imageField === "object" && imageField !== null && "url" in imageField) {
-      return imageField.url;
-    }
-    return "";
-  };
-
+const Hero = ({ content }: HeroProps) => {
   return (
     <section className="relative w-full pt-40 pb-4 bg-(--cream) flex flex-col items-center">
       <div className="z-10 flex flex-col w-full max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -24,6 +17,23 @@ const Hero = async ({ content }: HeroProps) => {
             <h1 className="text-(--navy) font-semibold! text-4xl! m-0!">About Us</h1>
             <p className="text-(--navy) text-base">{content.description}</p>
           </div>
+
+          <div>
+            {content.stickers?.map(
+              (item, index) =>
+                typeof item.sticker === "object" &&
+                item.sticker?.url && (
+                  <Image
+                    key={index}
+                    src={item.sticker.url}
+                    alt={item.sticker.alt || `sticker ${index}`}
+                    width={20}
+                    height={20}
+                  />
+                ),
+            )}
+          </div>
+
           {/* <div className="flex flex-row flex-wrap md:flex-nowrap justify-center lg:gap-4 gap-8 lg:items-start">
             {content.aboutUsStickers?.map((sticker, i) => {
               const url = getImageUrl(sticker["sticker-image"]);
@@ -45,8 +55,7 @@ const Hero = async ({ content }: HeroProps) => {
             })}
           </div> */}
         </div>
-
-        <hr className="mt-8 border-t-[1.5px] border-[var(--navy)]" />
+        <div className="h-px bg-(--navy)" />
       </div>
     </section>
   );
