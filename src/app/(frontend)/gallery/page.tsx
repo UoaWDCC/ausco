dafe56b631b6ba7879fdc3f2b99ed13a3a624e01
vincenttao04 <vincent.jh.tao@gmail.com>
2025-12-09 22:27:32 +1300

    chore: clean up

--- src/app/(frontend)/gallery/page.tsx
+++ src/app/(frontend)/gallery/page.tsx
@@ -1,18 +1,7 @@
-import { Media } from "@/payload-types";
 import Image from "next/image";
 import Link from "next/link";
 
 import { getGalleryPage } from "@/actions/pageActions";
-
-const getImageUrl = (imageField: string | Media | null | undefined): string | undefined => {
-  if (typeof imageField === "object" && imageField !== null && "url" in imageField) {
-    return imageField.url ?? undefined;
-  }
-  if (typeof imageField === "string") {
-    return imageField;
-  }
-  return undefined; // Return undefined if no image is found
-};
 
 export default async function Gallery() {
   const content = await getGalleryPage();
