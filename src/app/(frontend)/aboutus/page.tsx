15862b53b83dacf4fbece250afc92c157d9097bf
vincenttao04 <vincent.jh.tao@gmail.com>
2025-12-19 14:05:12 +1300

    chore: rearrange paddings/widths

--- src/app/(frontend)/aboutus/page.tsx
+++ src/app/(frontend)/aboutus/page.tsx
@@ -7,9 +7,11 @@ export default async function AboutPage() {
   const aboutUsContent = await getAboutUsPage();
 
   return (
-    <div className="bg-(--cream)">
-      <Hero content={aboutUsContent.hero} />
-      <CardsSection content={aboutUsContent.cards} />
-    </div>
+    <section className="bg-(--cream)">
+      <div className="w-full max-w-6xl mx-auto pt-44 pb-16 px-6 flex flex-col items-center">
+        <Hero content={aboutUsContent.hero} />
+        <CardsSection content={aboutUsContent.cards} />
+      </div>
+    </section>
   );
 }
