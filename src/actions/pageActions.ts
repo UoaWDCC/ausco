16c8861ab514c9a4734fb1a32ea8e4740bfa77e1
vincenttao04 <vincent.jh.tao@gmail.com>
2025-12-21 23:27:20 +1300

    fix: payload error

--- src/actions/pageActions.ts
+++ src/actions/pageActions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { cache } from "react";
-import payload from "payload";
+import { getPayload } from "@libs/payload";
 
 import {
   Home,
@@ -15,35 +15,43 @@ import {
 } from "@/payload-types";
 
 export const getHome = cache(async (): Promise<Home> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "home", depth: 1 });
 });
 
 export const getAboutUs = cache(async (): Promise<AboutUs> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "about-us", depth: 1 });
 });
 
 export const getOurStory = cache(async (): Promise<OurStory> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "our-story", depth: 1 });
 });
 
 // Payload Globals are single documents, so the generated TS type is singular (OurPerson) even if the slug is plural (our-people).
 // See src\collections\global\OurPeople.ts docs for more information.
 export const getOurPeople = cache(async (): Promise<OurPerson> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "our-people", depth: 1 });
 });
 
 export const getConcerts = cache(async (): Promise<Concert> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "concerts", depth: 1 });
 });
 
 export const getConcertsUpcoming = cache(async (): Promise<ConcertsUpcoming> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "concerts-upcoming", depth: 1 });
 });
 
 export const getConcertsPast = cache(async (): Promise<ConcertsPast> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "concerts-past", depth: 1 });
 });
 
 export const getGallery = cache(async (): Promise<Gallery> => {
+  const payload = await getPayload();
   return payload.findGlobal({ slug: "gallery", depth: 1 });
 });
